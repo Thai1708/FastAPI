ce1ced6edd95de3975c748a19afe5c5e65e8f6a7
MinhThai <minhthai170807@gmail.com>
2025-10-04 17:04:55 +0700

    update production code

--- boot/docker-run.sh
+++ boot/docker-run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -e
 
 source /opt/venv/bin/activate
-
 cd /code
-RUN_PORT=${PORT:-8000}
+
+RUN_PORT=${PORT:-8002}
 RUN_HOST=${HOST:-0.0.0.0}
 
-gunicorn -k uvicorn.workers.UvicornWorker -b $RUN_HOST:$RUN_PORT main:app 
+exec gunicorn -k uvicorn.workers.UvicornWorker -b $RUN_HOST:$RUN_PORT main:app
